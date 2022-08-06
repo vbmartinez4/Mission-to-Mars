9896cd94f68d376aead58fe53151464663f06ff8
vbmartinez4 <vbmartinez15@gmail.com>
2022-08-06 13:39:17 -0400

    Integrate MongoDB into the Web App.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Import dependencies
 from flask import Flask, render_template, redirect, url_for
-from flask_pymongo import Pymongo
+from flask_pymongo import PyMongo
 import scraping
 
 # Set up Flask
--- scraping.py
+++ scraping.py
@@ -1,12 +1,32 @@
-# Import Splinter and BeautifulSoup
+# Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
-# Set up Splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+
+def scrape_all():
+
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+    
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
+
 
 # ### The News: Titles and Summaries
 def mars_news(browser):
@@ -82,6 +102,9 @@ def mars_facts():
     df.set_index('Description',inplace=True)
 
     # Conver the DataFrame into HTML format, add bootstrap
-    return df.to_html()
+    return df.to_html(classes="table table-striped")
 
-    browser.quit()
+if __name__ == "__main__":
+
+    # If running as script, print scraped data
+    print(scrape_all())
